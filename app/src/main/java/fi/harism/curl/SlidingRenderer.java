b8b9d5a494adc1a59a94dc14471ad1b3cd9e6391
p_zlulzhang <zhanglulu@yuewen.com>
2018-12-18 19:48:41 +0800

    添加滑动渲染器

---
+++ app/src/main/java/fi/harism/curl/SlidingRenderer.java
@@ -0,0 +1,50 @@
+/*
+   Copyright 2012 Harri Smatt
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package fi.harism.curl;
+
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+
+import java.util.Vector;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * 滑动动画渲染器.
+ * 
+ * @author p_zlulzhang
+ */
+public class SlidingRenderer implements GLSurfaceView.Renderer {
+	@Override
+	public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+	}
+
+	@Override
+	public void onDrawFrame(GL10 gl10) {
+
+	}
+}
